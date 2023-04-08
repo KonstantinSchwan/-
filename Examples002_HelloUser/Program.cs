3f0d2e0bba4b7a93515847de1ad1d5e874cb0b3f
KonstantinSchwan <konstantinschwan@gmx.de>
2023-04-08 18:27:52 +0200

    изменил гитигнор

---
+++ Examples002_HelloUser/Program.cs
@@ -0,0 +1,6 @@
+﻿//Console.OutputEncoding = System.Text.Encoding.UTF8;
+Console.InputEncoding = System.Text.Encoding.Unicode;
+Console.Write("Введите ваше имя ");
+string username = Console.ReadLine();
+Console.Write("Привет, ");
+Console.Write(username);
